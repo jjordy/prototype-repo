5dc9dc76ee62ebebda51de715277d585c115c1f9
Jordan Addison <jordanrileyaddison@gmail.com>
2023-01-27 00:16:45 -0600

    Initial commit

---
+++ packages/data/seed.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      name: "Alice",
+      address_1: "test",
+      zip_code: "39507",
+      tickets: {
+        create: {
+          title: "Ticket 1",
+          status: "TODO",
+        },
+      },
+    },
+  });
+
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      name: "Bob",
+    },
+  });
+  console.log({ alice, bob });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
