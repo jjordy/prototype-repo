4c1db43dd165ce6bd6debe894244f4cf8453cba0
Jordan Addison <jordanrileyaddison@gmail.com>
2023-02-02 21:52:47 -0600

    Everything works.

---
+++ apps/web/src/pages/sign-in/index.tsx
@@ -0,0 +1,11 @@
+import "@jjordy/ui/styles.css";
+
+import { Button } from "@jjordy/ui";
+
+export default function SignIn() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -1,5 +1,1 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-export default prisma;
-export { Prisma } from "@prisma/client";
+export * from "./client";
